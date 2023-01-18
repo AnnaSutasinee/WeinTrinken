cd4db3dd049e46e6402c06f371b5bced42195fef
AnnaSutasinee <103565265+AnnaSutasinee@users.noreply.github.com>
2023-01-18 23:14:01 +0100

    Testklasse fertig

--- 700.swen1.labor/src/test/java/patient/TestPatient.java
+++ 700.swen1.labor/src/test/java/patient/TestPatient.java
@@ -1,8 +1,5 @@
 package patient;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.time.LocalDateTime;
 
 import org.junit.Before;
@@ -12,11 +9,17 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import static org.junit.Assert.*;
+
 public class TestPatient {
 	// TODO
     @InjectMocks Patient p;
 
    @Mock AppointmentService as;
+
+   @Mock Appointment a;
+
+   @Mock Appointment b;
 
    @Before
     public void setUp() {
@@ -26,7 +29,44 @@ public class TestPatient {
 
    @Test(timeout = 1000)
     public void validID() {
+       boolean have = new Patient("Rofl", 23).getId() > 0;
+       assertTrue(have);
 
+   }
+
+   @Test(timeout=1000, expected = IllegalArgumentException.class)
+    public void isNameValid() {
+       p = new Patient(null, 23);
+
+   }
+
+   @Test(timeout=1000, expected = IllegalArgumentException.class)
+    public void isAgeValid() {
+       p = new Patient("Flo", -1);
+   }
+
+   @Test (timeout=1000)
+    public void addAppointmentValid() {
+       LocalDateTime time = LocalDateTime.now();
+       boolean have = p.addAppointment(time, time.plusMinutes(10));
+       assertTrue(have);
+   }
+
+   @Test (timeout=1000)
+    public void isAppointmentValid() {
+       LocalDateTime time = LocalDateTime.now();
+       boolean have = p.addAppointment(time, time.minusMinutes(10));
+       assertFalse(have);
+   }
+
+   @Test (timeout=1000)
+    public void averageDurationValid() {
+       Mockito.when(a.duration()).thenReturn(10);
+       Mockito.when(b.duration()).thenReturn(0);
+
+       int want = 5;
+       int have = p.getAverageWaitingTime();
+       assertEquals(want, have);
    }
 
 
